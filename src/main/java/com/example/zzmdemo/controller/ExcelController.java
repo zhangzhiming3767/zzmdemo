e4dd302e08c89b838d299fd2c552c2f529a4b30b
zhangzhiming <zhang.zhiming111@ztesoft.com>
2020-01-17 19:50:45 +0800

    ##https忽略证书

--- src/main/java/com/example/zzmdemo/controller/ExcelController.java
+++ src/main/java/com/example/zzmdemo/controller/ExcelController.java
@@ -12,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Date;
 import java.util.HashMap;
@@ -42,7 +48,7 @@ public class ExcelController {
         Map<String, Object> params = new HashMap<>();
         params.put("title111", "这是标题");
         params.put("name111", "李四");
-        params.put("test", new Date());
+//        params.put("test", null);
         //
         EasyPoiUtils.exportWord("word/aaa.docx", "E:/1/00Ztesoft/余杭", "aaa.docx", params, request, response);
     }
@@ -57,6 +63,76 @@ public class ExcelController {
             workbook.write(response.getOutputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+    /**
+    * @author :zhangzhiming
+    * description :请求https路径报错说 证书问题的，设置为忽略
+    * @date :Create in  2020/1/17 19:49
+    */
+    public void test() throws Exception {
+        trustAllHttpsCertificates();
+        HttpURLConnection connection = (HttpURLConnection) new URL("https://yhdjtest.oss-cn-hangzhou.aliyuncs.com/yhdjexample/20200102/c79b1e0b-eaab-458e-b618-fc1b69e282eb.png").openConnection();
+        connection.setReadTimeout(5000);
+        connection.setConnectTimeout(5000);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setUseCaches(false);
+        ((HttpsURLConnection) connection).setHostnameVerifier(new CustomizedHostnameVerifier());
+        connection.setRequestMethod("GET");
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            InputStream inputStream = connection.getInputStream();
+        }
+    }
+
+    private static void trustAllHttpsCertificates() throws Exception {
+        javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];
+        javax.net.ssl.TrustManager tm = new miTM();
+        trustAllCerts[0] = tm;
+        javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, null);
+        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+    static class miTM implements javax.net.ssl.TrustManager, javax.net.ssl.X509TrustManager {
+        @Override
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        public boolean isServerTrusted(java.security.cert.X509Certificate[] certs) {
+            return true;
+        }
+
+        public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {
+            return true;
+        }
+        @Override
+        public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                throws java.security.cert.CertificateException {
+            return;
+        }
+        @Override
+        public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                throws java.security.cert.CertificateException {
+            return;
+        }
+    }
+
+    /**
+     * 自定义验证类
+     */
+    public static class CustomizedHostnameVerifier implements HostnameVerifier {
+        /**
+         * 重写验证方法
+         *
+         * @param arg0
+         * @param arg1
+         * @return
+         */
+        @Override
+        public boolean verify(String arg0, SSLSession arg1) {
+            //所有都正确
+            return true;
         }
     }
 
