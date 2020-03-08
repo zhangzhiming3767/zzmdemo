7f1a18aad6fff268a4db0d5098123c89fa93c8a1
zhangzhiming <zhang.zhiming111@ztesoft.com>
2020-03-08 19:39:42 +0800

    ##KMP算法

--- src/main/java/com/example/zzmdemo/nothingtodowithproject/KMP.java
+++ src/main/java/com/example/zzmdemo/nothingtodowithproject/KMP.java
@@ -1,5 +1,7 @@
 package com.example.zzmdemo.nothingtodowithproject;
 
+import java.util.Arrays;
+
 /**
  * 应用模块名称<p>
  * 代码描述<p>   字符串匹配算法-KMP,不同于indexof的暴力破解法
@@ -12,7 +14,30 @@ package com.example.zzmdemo.nothingtodowithproject;
 public class KMP {
 
     public static void main(String[] args) {
-        System.out.println(kmpMatch("abcabaabaabcacb", "abaabcac"));
+        getNext("abaabcac");
+        getNextArray("abaabcac".toCharArray());
+//        System.out.println(kmpMatch("abcabaabaabcacb", "abaabcac"));
+    }
+    /**
+     * @author :zhangzhiming
+     * description :
+     * @date :Create in  2020/3/8 16:55
+     */
+    public static int[] getNext(String ps) {
+        char[] p = ps.toCharArray();
+        int[] next = new int[p.length];
+        next[0] = -1;
+        int j = 0;
+        int k = -1;
+        while (j < p.length - 1) {
+            if (k == -1 || p[j] == p[k]) {
+                next[++j] = ++k;
+            } else {
+                k = next[k];
+            }
+        }
+        System.out.println(Arrays.toString(next));
+        return next;
     }
     /**
      * 对主串s和模式串t进行KMP模式匹配
@@ -42,6 +67,39 @@ public class KMP {
     }
 
     /**
+    * @author :zhangzhiming
+    * description :根据理解，拆分一下，把上面的按照自己理解进行拆解
+    * @date :Create in  2020/3/8 19:32
+    */
+    public static int kmpMatchTest(String s, String t) {
+        char[] s_arr = s.toCharArray();
+        char[] t_arr = t.toCharArray();
+        int[] next = getNextArray(t_arr);
+        int i = 0, j = 0;
+        while (i < s_arr.length && j < t_arr.length) {
+            //如果是第一位不等，s往后取一位，j归零，也就是t去第一位，
+            //若是相等，则判断下一个是否相等，若相等，则再判断下一位，否则取一下j，来确定应该移动几位
+            if (j == -1 ) {
+                i++;
+                j=0;
+                //可以与下面的else合并，但不好理解
+            }else if(s_arr[i] == t_arr[j]){
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+        }
+        //t数组移动完了，说了每一项都匹配上了，那么开始的位置就是最后的位置i减去目标字段j的长度
+        if (j == t_arr.length) {
+            return i - j;
+        } else {
+            //要不然就是没找到
+            return -1;
+        }
+    }
+
+    /**
      * 求出一个字符数组的next数组
      *
      * @param t 字符数组
@@ -61,10 +119,13 @@ public class KMP {
                 } else {
                     k = next[k];
                 }
-                next[j] = 0;  //当k==-1而跳出循环时，next[j] = 0，否则next[j]会在break之前被赋值
+                //当k==-1而跳出循环时，next[j] = 0，否则next[j]会在break之前被赋值
+                next[j] = 0;
             }
         }
+        System.out.println(Arrays.toString(next));
         return next;
     }
+
 
 }
