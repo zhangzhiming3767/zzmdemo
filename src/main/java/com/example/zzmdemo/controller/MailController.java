7cc7c75c61627527802805b13ebb5e1c878cff84
zhangzhiming <zhang.zhiming111@ztesoft.com>
2020-03-29 20:22:55 +0800

    ## 发送QQ邮件,内容里加图片，附件

--- src/main/java/com/example/zzmdemo/controller/MailController.java
+++ src/main/java/com/example/zzmdemo/controller/MailController.java
@@ -2,7 +2,6 @@ package com.example.zzmdemo.controller;
 
 import com.example.zzmdemo.entity.response.Response;
 import com.example.zzmdemo.entity.response.SuccessResponse;
-import com.example.zzmdemo.nothingtodowithproject.lockTest.SynchronizedTest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,10 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.annotation.Resource;
-import javax.mail.*;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.FileOutputStream;
 import java.util.Properties;
 
 /**
@@ -32,6 +38,7 @@ public class MailController {
     private JavaMailSender javaMailSender;
     @Value("${spring.mail.username}")
     private String from;
+
     /**
      * 发送纯文本邮件.
      *
@@ -39,34 +46,32 @@ public class MailController {
      * @param subject 邮件主题
      * @param text    纯文本内容
      */
-    @PostMapping(value={"/sendEmail"})
-    public Response sendEmail(String to, String subject, String text)  {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        javaMailSender.send(message);
-        return new SuccessResponse();
-    }
+//    @PostMapping(value = {"/sendEmail"})
+//    public Response sendEmail(String to, String subject, String text) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//
+//        message.setFrom(from);
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(text);
+//        javaMailSender.send(message);
+//        return new SuccessResponse();
+//    }
 
     /**
-    * @author :zhangzhiming
-    * description :发送成功
+     * @author :zhangzhiming
+     * description :发送成功
      * to=2636971612@qq.com&subject=邮件主题&text=这是文本内容
-    * @date :Create in  2020/3/29 20:02
-    */
-    @PostMapping(value={"/sendEmailV2"})
-    public Response sendEmailV2(String to, String subject, String text) throws MessagingException {
+     * @date :Create in  2020/3/29 20:02
+     * @param type 邮件类型 1纯文本邮件
+     */
+    @PostMapping(value = {"/sendEmailV2"})
+    public Response sendEmailV2(String mailTo, String mailTitle, String mailText,String photoSrc,Integer type) throws Exception {
 
         // 返回创建好的邮件对象
         String mailFrom = "694332301@qq.com";
-        String passwordMailFrom="bhjroestjhdpbfjf";
-        String mailTo = to;
-        String mailTittle=subject;
-        String  mailText = text;
-        String mailHost="smtp.qq.com";
+        String passwordMailFrom = "bhjroestjhdpbfjf";
+        String mailHost = "smtp.qq.com";
         Properties prop = new Properties();
         prop.setProperty("mail.host", mailHost);
         prop.setProperty("mail.transport.protocol", "smtp");
@@ -79,21 +84,116 @@ public class MailController {
         // 2、通过session得到transport对象
         Transport ts = session.getTransport();
         // 3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
-        ts.connect(mailHost,mailFrom, passwordMailFrom);
-        // 4、创建邮件
+        ts.connect(mailHost, mailFrom, passwordMailFrom);
+        // 4、创建邮件 纯文字
+        Message message = null;
+        if(type==1){
+            message = createSimpleMail(session, mailFrom, mailTo, mailTitle, mailText);
+        }else if(type==2) {
+            message = createImageMail(session, mailFrom, mailTo, mailTitle, mailText,photoSrc);
+        }else {
+            message = createAttachMail(session, mailFrom, mailTo, mailTitle, mailText,photoSrc);
+        }
+        // 5、发送邮件
+        ts.sendMessage(message, message.getAllRecipients());
+        ts.close();
+        return new SuccessResponse();
+    }
+
+
+    /**
+     * @Method: createSimpleMail
+     * @Description: 创建一封只包含文本的邮件
+     */
+    public MimeMessage createSimpleMail(Session session, String mailfrom, String mailTo, String mailTittle,
+                                        String mailText) throws Exception {
         // 创建邮件对象
         MimeMessage message = new MimeMessage(session);
         // 指明邮件的发件人
-        message.setFrom(new InternetAddress(mailFrom));
+        message.setFrom(new InternetAddress(mailfrom));
         // 指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
         message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
         // 邮件的标题
         message.setSubject(mailTittle);
         // 邮件的文本内容
         message.setContent(mailText, "text/html;charset=UTF-8");
-        // 5、发送邮件
-        ts.sendMessage(message, message.getAllRecipients());
-        ts.close();
-        return new SuccessResponse();
+        // 返回创建好的邮件对象
+        return message;
+    }
+
+    /**
+     * @param session
+     * @return
+     * @throws Exception
+     * @Method: createImageMail
+     * @Description: 生成一封邮件正文带图片的邮件
+     */
+    public static MimeMessage createImageMail(Session session, String mailFrom, String mailTo, String mailTittle,
+                                              String mailText, String photoSrc) throws Exception {
+        // 创建邮件
+        MimeMessage message = new MimeMessage(session);
+        // 设置邮件的基本信息
+// 发件人
+        message.setFrom(new InternetAddress(mailFrom));
+        // 收件人
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
+        // 邮件标题
+        message.setSubject(mailTittle);
+        // 准备邮件数据
+        // 准备邮件正文数据
+        MimeBodyPart text = new MimeBodyPart();
+        // 需要修改
+        text.setContent(mailText + "<img src='cid:xxx.jpg'>", "text/html;charset=UTF-8");
+        // 准备图片数据
+        MimeBodyPart image = new MimeBodyPart();
+        // 需要修改
+        DataHandler dh = new DataHandler(new FileDataSource(photoSrc));
+        image.setDataHandler(dh);
+        // 需要修改
+        image.setContentID("xxx.jpg");
+        // 描述数据关系
+        MimeMultipart mm = new MimeMultipart();
+        mm.addBodyPart(text);
+        mm.addBodyPart(image);
+        mm.setSubType("related");
+        message.setContent(mm);
+        message.saveChanges();
+        // 将创建好的邮件写入到F盘以文件的形式进行保存
+//        message.writeTo(new FileOutputStream("F:/Program Files/TestMail/ImageMail.eml"));// 需要修改
+        // 返回创建好的邮件
+        return message;
+    }
+
+    /**
+     * @Method: createAttachMail
+     * @Description: 创建一封带附件的邮件
+     */
+    public static MimeMessage createAttachMail(Session session, String mailFrom, String mailTo, String mailTittle,
+                                               String mailText,String photoSrc) throws Exception {
+        MimeMessage message = new MimeMessage(session);
+        // 设置邮件的基本信息
+        message.setFrom(new InternetAddress(mailFrom));	// 发件人
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));// 收件人
+        // 邮件标题
+        message.setSubject(mailTittle);
+        // 创建邮件正文，为了避免邮件正文中文乱码问题，需要使用charset=UTF-8指明字符编码
+        MimeBodyPart text = new MimeBodyPart();
+        text.setContent(mailText, "text/html;charset=UTF-8");
+        // 创建邮件附件
+        MimeBodyPart attach = new MimeBodyPart();
+        DataHandler dh = new DataHandler(new FileDataSource(photoSrc));// 需要修改
+        attach.setDataHandler(dh);
+        attach.setFileName(dh.getName());
+        // 创建容器描述数据关系
+        MimeMultipart mp = new MimeMultipart();
+        mp.addBodyPart(text);
+        mp.addBodyPart(attach);
+        mp.setSubType("mixed");
+        message.setContent(mp);
+        message.saveChanges();
+        // 将创建的Email写入到F盘存储
+//        message.writeTo(new FileOutputStream("F:/Program Files/TestMail/ImageMail.eml"));// 需要修改
+        // 返回生成的邮件
+        return message;
     }
 }
